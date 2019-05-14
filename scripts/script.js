fb2a792688e5a4f6c69f85564d0c0e25b09cabc2
MaximKofanov <m.kofanov@gmail.com>
2019-05-14 21:10:38 +0300

    menu checkbox

--- scripts/script.js
+++ scripts/script.js
@@ -1,29 +1,36 @@
 'use strict';
 $(() => {
-  
-  let check = 1;
 
-  for (var key in products) {
+  for (let key in products) {
     $('.list-name').append('<li><input type="checkbox" name="' + key + '"> ' + key + '</li>');
-    $('.products').append('<li><img src="' + products[key].image + '" alt="' + key + '"><h3>' + key + '</h3><p>' + products[key].description + '</p><p class="product-cost">$' + products[key].cost + ' USD</p></li>');
+  }
+
+  for(let a = 0; a < tags.length; a++){
+    $('.list-tags').append('<li><input type="checkbox" name="' + tags[a] + '"> ' + tags[a] + '</li>');
   }
   
-  $('input[type="checkbox"]').click(function() {
-    if(check){
-      $('.products').empty();
-      check--;
-    }
-    for (var key in products) {
-      let keyName = $(this).attr('name');
+  $('.list-name input[type="checkbox"]').click(function() {
+    let keyName = $(this).attr('name');
+    for (let key in products) {
       if(key === keyName){
         if($(this).is(':checked')){
-          $('.products').append('<li class="'+ products[keyName].vendorCode +'"><img src="' + products[keyName].image + '" alt="' + keyName + '"><h3>' + keyName + '</h3><p>' + products[keyName].description + '</p><p class="product-cost">$' + products[keyName].cost + ' USD</p></li>');
+          $('.products').append('<li class="vc'+ products[keyName].vendorCode +' '+products[keyName].tags+'"><img src="' + products[keyName].image + '" alt="' + keyName + '"><h3>' + keyName + '</h3><p>' + products[keyName].description + '</p><p class="product-cost">$' + products[keyName].cost + ' USD</p></li>');
         }else{
-          $('.'+ products[keyName].vendorCode +'').remove();
+          $('.vc'+ products[keyName].vendorCode +'').remove();
         }
       }
     }
-
   });
-  
+
+  $('.list-tags input[type="checkbox"]').click(function() {
+    let keyTag = $(this).attr('name');
+    if($(this).is(':checked')){
+      $('.'+ keyTag +'').removeClass('display-none');
+    }else{
+      $('.'+ keyTag +'').addClass('display-none');
+    }
+  });
+      
+  $('.mnu input[type="checkbox"]').trigger('click');
+
 });  
