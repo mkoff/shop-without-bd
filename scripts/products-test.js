2ce7488b5475f791439b8f0ba03e580ad1476b48
MaximKofanov <m.kofanov@gmail.com>
2019-05-14 22:48:28 +0300

    menu checkbox fix

--- scripts/products-test.js
+++ scripts/products-test.js
@@ -419,6 +419,426 @@ const products = {
     tags : '64gb 128gb 256gb',
     vendorCode : 10006
   },
+    iphozne5s : {
+    image : 'images/5s.jpg',
+    description : '5sLorem ipsum dolor sit amet, consectetur adipisicing elit. Molestiae non ipsum qui laudantium voluptatibus ipsa pariatur sit facere, quaerat rerum!',
+    cost : 300,
+    tags : '16gb 32gb',
+    vendorCode : 10001
+  },
+  iphonzxe6 : {
+    image : 'images/6.jpg',
+    description : '6Lorem ipsum dolor sit amet, consectetur adipisicing elit. Molestiae non ipsum qui laudantium voluptatibus ipsa pariatur sit facere, quaerat rerum!',
+    cost : 400,
+    tags : '16gb 32gb 64gb',
+    vendorCode : 10002
+  },
+  iphonex6s : {
+    image : 'images/6s.jpg',
+    description : '6sLorem ipsum dolor sit amet, consectetur adipisicing elit. Molestiae non ipsum qui laudantium voluptatibus ipsa pariatur sit facere, quaerat rerum!',
+    cost : 450,
+    tags : '16gb 32gb 64gb',
+    vendorCode : 10003
+  },
+  iphonex7 : {
+    image : 'images/7.jpg',
+    description : '7Lorem ipsum dolor sit amet, consectetur adipisicing elit. Molestiae non ipsum qui laudantium voluptatibus ipsa pariatur sit facere, quaerat rerum!',
+    cost : 550,
+    tags : '16gb 32gb 64gb 128gb',
+    vendorCode : 10004
+  },
+  iphonez8 : {
+    image : 'images/8.jpg',
+    description : '8Lorem ipsum dolor sit amet, consectetur adipisicing elit. Molestiae non ipsum qui laudantium voluptatibus ipsa pariatur sit facere, quaerat rerum!',
+    cost : 650,
+    tags : '32gb 64gb 128gb',
+    vendorCode : 10005
+  },
+  iphonex10 : {
+    image : 'images/10.jpg',
+    description : '10Lorem ipsum dolor sit amet, consectetur adipisicing elit. Molestiae non ipsum qui laudantium voluptatibus ipsa pariatur sit facere, quaerat rerum!',
+    cost : 850,
+    tags : '64gb 128gb 256gb',
+    vendorCode : 10006
+  },
+  iphone5xx1s : {
+    image : 'images/5s.jpg',
+    description : '5sLorem ipsum dolor sit amet, consectetur adipisicing elit. Molestiae non ipsum qui laudantium voluptatibus ipsa pariatur sit facere, quaerat rerum!',
+    cost : 300,
+    tags : '16gb 32gb',
+    vendorCode : 10001
+  },
+  iphone6xx2 : {
+    image : 'images/6.jpg',
+    description : '6Lorem ipsum dolor sit amet, consectetur adipisicing elit. Molestiae non ipsum qui laudantium voluptatibus ipsa pariatur sit facere, quaerat rerum!',
+    cost : 400,
+    tags : '16gb 32gb 64gb',
+    vendorCode : 10002
+  },
+  iphone62xxs : {
+    image : 'images/6s.jpg',
+    description : '6sLorem ipsum dolor sit amet, consectetur adipisicing elit. Molestiae non ipsum qui laudantium voluptatibus ipsa pariatur sit facere, quaerat rerum!',
+    cost : 450,
+    tags : '16gb 32gb 64gb',
+    vendorCode : 10003
+  },
+  iphone7xxd : {
+    image : 'images/7.jpg',
+    description : '7Lorem ipsum dolor sit amet, consectetur adipisicing elit. Molestiae non ipsum qui laudantium voluptatibus ipsa pariatur sit facere, quaerat rerum!',
+    cost : 550,
+    tags : '16gb 32gb 64gb 128gb',
+    vendorCode : 10004
+  },
+  iphone85xx : {
+    image : 'images/8.jpg',
+    description : '8Lorem ipsum dolor sit amet, consectetur adipisicing elit. Molestiae non ipsum qui laudantium voluptatibus ipsa pariatur sit facere, quaerat rerum!',
+    cost : 650,
+    tags : '32gb 64gb 128gb',
+    vendorCode : 10005
+  },
+  iphone1zz01 : {
+    image : 'images/10.jpg',
+    description : '10Lorem ipsum dolor sit amet, consectetur adipisicing elit. Molestiae non ipsum qui laudantium voluptatibus ipsa pariatur sit facere, quaerat rerum!',
+    cost : 850,
+    tags : '64gb 128gb 256gb',
+    vendorCode : 10006
+  },
+  iphone5sxsd : {
+    image : 'images/5s.jpg',
+    description : '5sLorem ipsum dolor sit amet, consectetur adipisicing elit. Molestiae non ipsum qui laudantium voluptatibus ipsa pariatur sit facere, quaerat rerum!',
+    cost : 300,
+    tags : '16gb 32gb',
+    vendorCode : 10001
+  },
+  iphone6sasdf : {
+    image : 'images/6.jpg',
+    description : '6Lorem ipsum dolor sit amet, consectetur adipisicing elit. Molestiae non ipsum qui laudantium voluptatibus ipsa pariatur sit facere, quaerat rerum!',
+    cost : 400,
+    tags : '16gb 32gb 64gb',
+    vendorCode : 10002
+  },
+  iphone6sdsadf : {
+    image : 'images/6s.jpg',
+    description : '6sLorem ipsum dolor sit amet, consectetur adipisicing elit. Molestiae non ipsum qui laudantium voluptatibus ipsa pariatur sit facere, quaerat rerum!',
+    cost : 450,
+    tags : '16gb 32gb 64gb',
+    vendorCode : 10003
+  },
+  iphone7aasdf : {
+    image : 'images/7.jpg',
+    description : '7Lorem ipsum dolor sit amet, consectetur adipisicing elit. Molestiae non ipsum qui laudantium voluptatibus ipsa pariatur sit facere, quaerat rerum!',
+    cost : 550,
+    tags : '16gb 32gb 64gb 128gb',
+    vendorCode : 10004
+  },
+  iphonea8saf : {
+    image : 'images/8.jpg',
+    description : '8Lorem ipsum dolor sit amet, consectetur adipisicing elit. Molestiae non ipsum qui laudantium voluptatibus ipsa pariatur sit facere, quaerat rerum!',
+    cost : 650,
+    tags : '32gb 64gb 128gb',
+    vendorCode : 10005
+  },
+  iphone1f0314 : {
+    image : 'images/10.jpg',
+    description : '10Lorem ipsum dolor sit amet, consectetur adipisicing elit. Molestiae non ipsum qui laudantium voluptatibus ipsa pariatur sit facere, quaerat rerum!',
+    cost : 850,
+    tags : '64gb 128gb 256gb',
+    vendorCode : 10006
+  },
+  iphonef5sasdf : {
+    image : 'images/5s.jpg',
+    description : '5sLorem ipsum dolor sit amet, consectetur adipisicing elit. Molestiae non ipsum qui laudantium voluptatibus ipsa pariatur sit facere, quaerat rerum!',
+    cost : 300,
+    tags : '16gb 32gb',
+    vendorCode : 10001
+  },
+  iphone6fdh : {
+    image : 'images/6.jpg',
+    description : '6Lorem ipsum dolor sit amet, consectetur adipisicing elit. Molestiae non ipsum qui laudantium voluptatibus ipsa pariatur sit facere, quaerat rerum!',
+    cost : 400,
+    tags : '16gb 32gb 64gb',
+    vendorCode : 10002
+  },
+  iphone6saadsf : {
+    image : 'images/6s.jpg',
+    description : '6sLorem ipsum dolor sit amet, consectetur adipisicing elit. Molestiae non ipsum qui laudantium voluptatibus ipsa pariatur sit facere, quaerat rerum!',
+    cost : 450,
+    tags : '16gb 32gb 64gb',
+    vendorCode : 10003
+  },
+  iphone7wtd43 : {
+    image : 'images/7.jpg',
+    description : '7Lorem ipsum dolor sit amet, consectetur adipisicing elit. Molestiae non ipsum qui laudantium voluptatibus ipsa pariatur sit facere, quaerat rerum!',
+    cost : 550,
+    tags : '16gb 32gb 64gb 128gb',
+    vendorCode : 10004
+  },
+  iphone8sadsf24 : {
+    image : 'images/8.jpg',
+    description : '8Lorem ipsum dolor sit amet, consectetur adipisicing elit. Molestiae non ipsum qui laudantium voluptatibus ipsa pariatur sit facere, quaerat rerum!',
+    cost : 650,
+    tags : '32gb 64gb 128gb',
+    vendorCode : 10005
+  },
+  iphone1dsfag30 : {
+    image : 'images/10.jpg',
+    description : '10Lorem ipsum dolor sit amet, consectetur adipisicing elit. Molestiae non ipsum qui laudantium voluptatibus ipsa pariatur sit facere, quaerat rerum!',
+    cost : 850,
+    tags : '64gb 128gb 256gb',
+    vendorCode : 10006
+  },
+  iphone5ssafdg3 : {
+    image : 'images/5s.jpg',
+    description : '5sLorem ipsum dolor sit amet, consectetur adipisicing elit. Molestiae non ipsum qui laudantium voluptatibus ipsa pariatur sit facere, quaerat rerum!',
+    cost : 300,
+    tags : '16gb 32gb',
+    vendorCode : 10001
+  },
+  iphone6sdzfg3 : {
+    image : 'images/6.jpg',
+    description : '6Lorem ipsum dolor sit amet, consectetur adipisicing elit. Molestiae non ipsum qui laudantium voluptatibus ipsa pariatur sit facere, quaerat rerum!',
+    cost : 400,
+    tags : '16gb 32gb 64gb',
+    vendorCode : 10002
+  },
+  iphone6szfgs : {
+    image : 'images/6s.jpg',
+    description : '6sLorem ipsum dolor sit amet, consectetur adipisicing elit. Molestiae non ipsum qui laudantium voluptatibus ipsa pariatur sit facere, quaerat rerum!',
+    cost : 450,
+    tags : '16gb 32gb 64gb',
+    vendorCode : 10003
+  },
+  iphonec7sfg : {
+    image : 'images/7.jpg',
+    description : '7Lorem ipsum dolor sit amet, consectetur adipisicing elit. Molestiae non ipsum qui laudantium voluptatibus ipsa pariatur sit facere, quaerat rerum!',
+    cost : 550,
+    tags : '16gb 32gb 64gb 128gb',
+    vendorCode : 10004
+  },
+  iphone8sgd24 : {
+    image : 'images/8.jpg',
+    description : '8Lorem ipsum dolor sit amet, consectetur adipisicing elit. Molestiae non ipsum qui laudantium voluptatibus ipsa pariatur sit facere, quaerat rerum!',
+    cost : 650,
+    tags : '32gb 64gb 128gb',
+    vendorCode : 10005
+  },
+  iphone10sdffg : {
+    image : 'images/10.jpg',
+    description : '10Lorem ipsum dolor sit amet, consectetur adipisicing elit. Molestiae non ipsum qui laudantium voluptatibus ipsa pariatur sit facere, quaerat rerum!',
+    cost : 850,
+    tags : '64gb 128gb 256gb',
+    vendorCode : 10006
+  },
+  iphone5sdsdfg3 : {
+    image : 'images/5s.jpg',
+    description : '5sLorem ipsum dolor sit amet, consectetur adipisicing elit. Molestiae non ipsum qui laudantium voluptatibus ipsa pariatur sit facere, quaerat rerum!',
+    cost : 300,
+    tags : '16gb 32gb',
+    vendorCode : 10001
+  },
+  iphonea6sfg3 : {
+    image : 'images/6.jpg',
+    description : '6Lorem ipsum dolor sit amet, consectetur adipisicing elit. Molestiae non ipsum qui laudantium voluptatibus ipsa pariatur sit facere, quaerat rerum!',
+    cost : 400,
+    tags : '16gb 32gb 64gb',
+    vendorCode : 10002
+  },
+  iphone6assfdg : {
+    image : 'images/6s.jpg',
+    description : '6sLorem ipsum dolor sit amet, consectetur adipisicing elit. Molestiae non ipsum qui laudantium voluptatibus ipsa pariatur sit facere, quaerat rerum!',
+    cost : 450,
+    tags : '16gb 32gb 64gb',
+    vendorCode : 10003
+  },
+  iphoane7sfg : {
+    image : 'images/7.jpg',
+    description : '7Lorem ipsum dolor sit amet, consectetur adipisicing elit. Molestiae non ipsum qui laudantium voluptatibus ipsa pariatur sit facere, quaerat rerum!',
+    cost : 550,
+    tags : '16gb 32gb 64gb 128gb',
+    vendorCode : 10004
+  },
+  iphone8d234 : {
+    image : 'images/8.jpg',
+    description : '8Lorem ipsum dolor sit amet, consectetur adipisicing elit. Molestiae non ipsum qui laudantium voluptatibus ipsa pariatur sit facere, quaerat rerum!',
+    cost : 650,
+    tags : '32gb 64gb 128gb',
+    vendorCode : 10005
+  },
+  iphoner10adsf : {
+    image : 'images/10.jpg',
+    description : '10Lorem ipsum dolor sit amet, consectetur adipisicing elit. Molestiae non ipsum qui laudantium voluptatibus ipsa pariatur sit facere, quaerat rerum!',
+    cost : 850,
+    tags : '64gb 128gb 256gb',
+    vendorCode : 10006
+  },
+  iphone5xrvcbs : {
+    image : 'images/5s.jpg',
+    description : '5sLorem ipsum dolor sit amet, consectetur adipisicing elit. Molestiae non ipsum qui laudantium voluptatibus ipsa pariatur sit facere, quaerat rerum!',
+    cost : 300,
+    tags : '16gb 32gb',
+    vendorCode : 10001
+  },
+  iphoner6sfd : {
+    image : 'images/6.jpg',
+    description : '6Lorem ipsum dolor sit amet, consectetur adipisicing elit. Molestiae non ipsum qui laudantium voluptatibus ipsa pariatur sit facere, quaerat rerum!',
+    cost : 400,
+    tags : '16gb 32gb 64gb',
+    vendorCode : 10002
+  },
+  iphonrdfhe6s : {
+    image : 'images/6s.jpg',
+    description : '6sLorem ipsum dolor sit amet, consectetur adipisicing elit. Molestiae non ipsum qui laudantium voluptatibus ipsa pariatur sit facere, quaerat rerum!',
+    cost : 450,
+    tags : '16gb 32gb 64gb',
+    vendorCode : 10003
+  },
+  iphsrfdgone7 : {
+    image : 'images/7.jpg',
+    description : '7Lorem ipsum dolor sit amet, consectetur adipisicing elit. Molestiae non ipsum qui laudantium voluptatibus ipsa pariatur sit facere, quaerat rerum!',
+    cost : 550,
+    tags : '16gb 32gb 64gb 128gb',
+    vendorCode : 10004
+  },
+  iphonsrfdge8 : {
+    image : 'images/8.jpg',
+    description : '8Lorem ipsum dolor sit amet, consectetur adipisicing elit. Molestiae non ipsum qui laudantium voluptatibus ipsa pariatur sit facere, quaerat rerum!',
+    cost : 650,
+    tags : '32gb 64gb 128gb',
+    vendorCode : 10005
+  },
+  iphosdfgnre10 : {
+    image : 'images/10.jpg',
+    description : '10Lorem ipsum dolor sit amet, consectetur adipisicing elit. Molestiae non ipsum qui laudantium voluptatibus ipsa pariatur sit facere, quaerat rerum!',
+    cost : 850,
+    tags : '64gb 128gb 256gb',
+    vendorCode : 10006
+  },
+  iphone5rr23s : {
+    image : 'images/5s.jpg',
+    description : '5sLorem ipsum dolor sit amet, consectetur adipisicing elit. Molestiae non ipsum qui laudantium voluptatibus ipsa pariatur sit facere, quaerat rerum!',
+    cost : 300,
+    tags : '16gb 32gb',
+    vendorCode : 10001
+  },
+  iphonesrfg6 : {
+    image : 'images/6.jpg',
+    description : '6Lorem ipsum dolor sit amet, consectetur adipisicing elit. Molestiae non ipsum qui laudantium voluptatibus ipsa pariatur sit facere, quaerat rerum!',
+    cost : 400,
+    tags : '16gb 32gb 64gb',
+    vendorCode : 10002
+  },
+  iphonsgre6s : {
+    image : 'images/6s.jpg',
+    description : '6sLorem ipsum dolor sit amet, consectetur adipisicing elit. Molestiae non ipsum qui laudantium voluptatibus ipsa pariatur sit facere, quaerat rerum!',
+    cost : 450,
+    tags : '16gb 32gb 64gb',
+    vendorCode : 10003
+  },
+  iphonsfrge7 : {
+    image : 'images/7.jpg',
+    description : '7Lorem ipsum dolor sit amet, consectetur adipisicing elit. Molestiae non ipsum qui laudantium voluptatibus ipsa pariatur sit facere, quaerat rerum!',
+    cost : 550,
+    tags : '16gb 32gb 64gb 128gb',
+    vendorCode : 10004
+  },
+  iphonasrq23e8 : {
+    image : 'images/8.jpg',
+    description : '8Lorem ipsum dolor sit amet, consectetur adipisicing elit. Molestiae non ipsum qui laudantium voluptatibus ipsa pariatur sit facere, quaerat rerum!',
+    cost : 650,
+    tags : '32gb 64gb 128gb',
+    vendorCode : 10005
+  },
+  ipho2134newe10 : {
+    image : 'images/10.jpg',
+    description : '10Lorem ipsum dolor sit amet, consectetur adipisicing elit. Molestiae non ipsum qui laudantium voluptatibus ipsa pariatur sit facere, quaerat rerum!',
+    cost : 850,
+    tags : '64gb 128gb 256gb',
+    vendorCode : 10006
+  },
+  iphonee532652s : {
+    image : 'images/5s.jpg',
+    description : '5sLorem ipsum dolor sit amet, consectetur adipisicing elit. Molestiae non ipsum qui laudantium voluptatibus ipsa pariatur sit facere, quaerat rerum!',
+    cost : 300,
+    tags : '16gb 32gb',
+    vendorCode : 10001
+  },
+  iphon3sdshfe6 : {
+    image : 'images/6.jpg',
+    description : '6Lorem ipsum dolor sit amet, consectetur adipisicing elit. Molestiae non ipsum qui laudantium voluptatibus ipsa pariatur sit facere, quaerat rerum!',
+    cost : 400,
+    tags : '16gb 32gb 64gb',
+    vendorCode : 10002
+  },
+  iphoned3gh356s : {
+    image : 'images/6s.jpg',
+    description : '6sLorem ipsum dolor sit amet, consectetur adipisicing elit. Molestiae non ipsum qui laudantium voluptatibus ipsa pariatur sit facere, quaerat rerum!',
+    cost : 450,
+    tags : '16gb 32gb 64gb',
+    vendorCode : 10003
+  },
+  iphont37 : {
+    image : 'images/7.jpg',
+    description : '7Lorem ipsum dolor sit amet, consectetur adipisicing elit. Molestiae non ipsum qui laudantium voluptatibus ipsa pariatur sit facere, quaerat rerum!',
+    cost : 550,
+    tags : '16gb 32gb 64gb 128gb',
+    vendorCode : 10004
+  },
+  ipho3ne8234 : {
+    image : 'images/8.jpg',
+    description : '8Lorem ipsum dolor sit amet, consectetur adipisicing elit. Molestiae non ipsum qui laudantium voluptatibus ipsa pariatur sit facere, quaerat rerum!',
+    cost : 650,
+    tags : '32gb 64gb 128gb',
+    vendorCode : 10005
+  },
+  iphone3asdf10 : {
+    image : 'images/10.jpg',
+    description : '10Lorem ipsum dolor sit amet, consectetur adipisicing elit. Molestiae non ipsum qui laudantium voluptatibus ipsa pariatur sit facere, quaerat rerum!',
+    cost : 850,
+    tags : '64gb 128gb 256gb',
+    vendorCode : 10006
+  },
+  iphoxbne53s : {
+    image : 'images/5s.jpg',
+    description : '5sLorem ipsum dolor sit amet, consectetur adipisicing elit. Molestiae non ipsum qui laudantium voluptatibus ipsa pariatur sit facere, quaerat rerum!',
+    cost : 300,
+    tags : '16gb 32gb',
+    vendorCode : 10001
+  },
+  iphonsf3ge6 : {
+    image : 'images/6.jpg',
+    description : '6Lorem ipsum dolor sit amet, consectetur adipisicing elit. Molestiae non ipsum qui laudantium voluptatibus ipsa pariatur sit facere, quaerat rerum!',
+    cost : 400,
+    tags : '16gb 32gb 64gb',
+    vendorCode : 10002
+  },
+  iphonwete36s : {
+    image : 'images/6s.jpg',
+    description : '6sLorem ipsum dolor sit amet, consectetur adipisicing elit. Molestiae non ipsum qui laudantium voluptatibus ipsa pariatur sit facere, quaerat rerum!',
+    cost : 450,
+    tags : '16gb 32gb 64gb',
+    vendorCode : 10003
+  },
+  iphonesfgd3gsh7 : {
+    image : 'images/7.jpg',
+    description : '7Lorem ipsum dolor sit amet, consectetur adipisicing elit. Molestiae non ipsum qui laudantium voluptatibus ipsa pariatur sit facere, quaerat rerum!',
+    cost : 550,
+    tags : '16gb 32gb 64gb 128gb',
+    vendorCode : 10004
+  },
+  iphonxzb3xve8 : {
+    image : 'images/8.jpg',
+    description : '8Lorem ipsum dolor sit amet, consectetur adipisicing elit. Molestiae non ipsum qui laudantium voluptatibus ipsa pariatur sit facere, quaerat rerum!',
+    cost : 650,
+    tags : '32gb 64gb 128gb',
+    vendorCode : 10005
+  },
+  iphone10x3cbvxb : {
+    image : 'images/10.jpg',
+    description : '10Lorem ipsum dolor sit amet, consectetur adipisicing elit. Molestiae non ipsum qui laudantium voluptatibus ipsa pariatur sit facere, quaerat rerum!',
+    cost : 850,
+    tags : '64gb 128gb 256gb',
+    vendorCode : 10006
+  },
 }
 
 const tags = ['16gb','32gb','64gb','128gb','256gb'];
--- scripts/script.js
+++ scripts/script.js
@@ -11,16 +11,21 @@ $(() => {
   
   $('.list-name input[type="checkbox"]').click(function() {
     let keyName = $(this).attr('name');
+    let counter = 0;
     for (let key in products) {
+      counter++;
       if(key === keyName){
         if($(this).is(':checked')){
-          $('.products').append('<li class="vc'+ products[keyName].vendorCode +' '+products[keyName].tags+'"><img src="' + products[keyName].image + '" alt="' + keyName + '"><h3>' + keyName + '</h3><p>' + products[keyName].description + '</p><p class="product-cost">$' + products[keyName].cost + ' USD</p></li>');
+          $('.products').append('<li class="vc'+ products[keyName].vendorCode +' '+products[keyName].tags+'"><img src="' + products[keyName].image + '" alt="' + keyName + '"><h3>' + keyName + '</h3><p class="product-description">' + products[keyName].description + '</p><p class="product-cost">$' + products[keyName].cost + ' USD</p></li>');
         }else{
           $('.vc'+ products[keyName].vendorCode +'').remove();
         }
       }
     }
+    console.log('product = '+counter);
   });
+
+
 
   $('.list-tags input[type="checkbox"]').click(function() {
     let keyTag = $(this).attr('name');
@@ -31,7 +36,23 @@ $(() => {
       $('.products li').removeClass('display-none');
     }
   });
-      
+
+  $('#removeAll').click(function(){
+    $('.mnu input:checkbox:checked').each(function(){
+        $(this).trigger('click');
+    });
+  });
+ 
   $('.list-name input[type="checkbox"]').trigger('click');
+
+  $('.products li').click(function(){
+    if($(this).hasClass('product-description-all')){
+      $(this).find('p:first').addClass('product-description');
+      $(this).removeClass('product-description-all');
+    }else{
+      $(this).addClass('product-description-all');
+      $(this).find('.product-description').removeClass('product-description');
+  }
+  });
 
 });  
